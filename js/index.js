370aa466e5183221d108bc2fdefb4d07ab9e4303
Elizabeth <elizabethallen36@gmail.com>
2021-02-24 21:10:12 -0800

    added code from class in the js file

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+document.body.innerHTML="hello world";
+
+var myName= "zephyr" ; //string
+var myName = "Z";
+
+let myNumber= "3" //number
+myNumber="6"
+
+const myBoolean = true; //boolean
+
+//complex
+const object = {
+    property1: true,
+    property2: 'string',
+    property3: 1,
+};
+const array = [1, 'string', true, object];
+
+//arrays are just objects with a more strutured order
+
+console.dir(document.body);
+
+//document.
+document.querySelector('UL').innerHTML= '<li>bye</li>'
+setTimeout(() => {
+    
+}, timeout);
